c93f0d85f0cebdc7d3a60e8634d710e8db647efe
kvimber <kevin.imber@circleci.com>
2022-05-17 16:45:21 -0700

    dict: Add merge conflict & crazy merge behavior

--- test/collections/dict_test.py
+++ test/collections/dict_test.py
@@ -1,8 +1,9 @@
 
+
 def test_dict_merge_operator():
     """
     AskPython says the BEST way to merge dicts is using the merge operator:
-      https://www.askpython.com/python/dictionary/merge-dictionaries
+      1. https://www.askpython.com/python/dictionary/merge-dictionaries
 
     Also gives 3 other methods I haven't mentioned here.
     """
@@ -13,5 +14,24 @@ def test_dict_merge_operator():
 
     assert dict1 == { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6 }
 
-# TODO what about merge conflicts?
+
+def test_dict_merge_conflicts():
+    dict1 = { 'a': 1, 'b': 2, 'c': 3 } # notice that both dicts have a 'c' key
+    dict2 = { 'd': 4, 'e': 5, 'c': 6 }
+
+    dict1 |= dict2 # looks like the right-side overrides in a collision
+    assert dict1['c'] == 6 # verify this
+
+
+def test_dict_merge_insanity():
+    """
+    Same AskPython article[1] has a different way to merge cards: by unpacking
+    them using `kwargs`!!!
+    """
+    dict1 = { 'a': 1, 'b': 2, 'c': 3 }
+    dict2 = { 'd': 4, 'e': 5, 'f': 6 }
+
+    dict3 = {**dict1, **dict2}
+
+    assert dict3 == { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6 }
 
